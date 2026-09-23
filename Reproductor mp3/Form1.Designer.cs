a35fa641f5fe1f187244e56c9701fc44f8aaafb1
icampos7-beep <icampos7@ucol.mx>
2026-09-23 01:28:16 -0600

    Update Form1.Designer.cs

--- Reproductor mp3/Form1.Designer.cs
+++ Reproductor mp3/Form1.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace Reproductor_mp3
+﻿﻿namespace reproductor_MP3
 {
     partial class Form1
     {
         /// <summary>
-        /// Variable del diseñador necesaria.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpiar los recursos que se estén usando.
+        ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,45 +20,141 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.lblTitulo = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
             // 
-            // lblTitulo
+            // label1
             // 
-            this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(86, 36);
-            this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(207, 22);
-            this.lblTitulo.TabIndex = 0;
-            this.lblTitulo.Text = "Titulo de la aplicacion";
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(39, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Archivo seleccionado";
+            label1.Click += label1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(96, 298);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(270, 314);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 15);
+            label4.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.musica1;
+            pictureBox1.Location = new Point(270, 123);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(205, 172);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(40, 58);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(401, 33);
+            panel1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(200, 15);
+            label2.TabIndex = 0;
+            label2.Text = "No hay ningun archivo seleccionado";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.OliveDrab;
+            button1.Location = new Point(173, 338);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 43);
+            button1.TabIndex = 7;
+            button1.Text = "Play";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkRed;
+            button2.Location = new Point(473, 338);
+            button2.Name = "button2";
+            button2.Size = new Size(97, 43);
+            button2.TabIndex = 8;
+            button2.Text = "Stop";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblTitulo);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.Text = "Resproductor mp3";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Reproducto Mp3";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.Label lblTitulo;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
     }
 }
-
